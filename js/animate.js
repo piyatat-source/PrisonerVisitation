25e637e6388bf0d94d5e17aa9523b116b9529889
Withan <withan@kkumail.com>
2020-11-20 03:34:02 +0700

    edit js

--- js/animate.js
+++ js/animate.js
@@ -13,25 +13,3 @@ var acc = document.getElementsByClassName("bottom-menu-submenu-title");
             } 
         });
         }
-
-
-//Slide
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" slide-active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " slide-active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
